0dcb3ed1e254fb029f2eca592e9809d750de9fb0
Zachary Khan <zacharythekhan@gmail.com>
2021-06-29 12:11:18 -0500

    basic script for adding each card describing the fetched markets

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,6 +4,7 @@ var ingredients = "broccoli"
 var recipeKey = "ed84eec3dc524169bf8954cb1aa495ef";
 var ingredients = "broccoli";
 var searchBtn = $('#search-btn');
+var marketCardContainer = $(".card-container"); // update with whatever html the market cards are made of
 
 function getResults(zip) {
     fetch("http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + zip)
@@ -44,11 +45,20 @@ function getRecipe() {
         })
 }
 
+function addMarketCards () {
+    for (i = 0; i < details.length; i++)
+        marketCardContainer.append("<div>"); // append new div in cardcontainer
+        marketCardContainer.children().eq(i).addClass("card-box"); // add box class to div
+        // append content elements within this new div
+        // append appropriate classes to style them
+}
+
 searchBtn.on("click", function() {
     var zip = $("#zip-in").val();
     console.log(zip);
     
-    getResults(zip);
-    getRecipe();
-});
+    addMarketCards();
 
+    getResults(zip);
+    //getRecipe();
+});
